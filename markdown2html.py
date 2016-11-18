b8bf7db430479fbad5f9165972117cd287c96100
Kenan Deng <dknyxh@gmail.com>
2016-11-18 15:18:06 -0500

    Move test code to test.py

--- markdown2html.py
+++ markdown2html.py
@@ -169,13 +169,6 @@ class MDListTag(MDRegexTag):
                     self.capturedStrStack[-1][-1] += '\n'
                     return ("", True, True)
                 else:
-#                    self.escaping = False
-#                    self._rewindStackToIndentLevel(newIndent)
-#                    if len(self.indentStack) == 0:
-#                        return_str = self.returnStr
-#                        self._reset()
-#                        return (return_str, False, False)
-#                    else:
                      self.capturedStrStack[-1][-1] += origStr.lstrip()
                      return ("", True, True)
             elif matchObj is None and self.escaping:
@@ -191,12 +184,12 @@ class MDListTag(MDRegexTag):
                 if newIndent < self.indentStack[-1][0]:
                     if self._insertCurrentOrRewind(newIndent):
                         self._rewindStackToIndentLevel(newIndent)
-                        if len(self.indentStack) == 0:
-                            self._pushStackToIndentLevel(newIndent, newString, symbols)
-                        if newIndent > self.indentStack[-1][0]:
-                            self._pushStackToIndentLevel(newString, newString, symbols)
-                        elif newIndent == self.indentStack[-1][0]:
-                            self._insertOnCurrentStack(newString)
+#                        if len(self.indentStack) == 0:
+#                            self._pushStackToIndentLevel(newIndent, newString, symbols)
+#                        if newIndent > self.indentStack[-1][0]:
+#                            self._pushStackToIndentLevel(newIndent, newString, symbols)
+#                        elif newIndent == self.indentStack[-1][0]:
+                        self._insertOnCurrentStack(newString)
                     else:
                         self._insertOnCurrentStack(newString)
                 elif newIndent == self.indentStack[-1][0]:
@@ -266,8 +259,9 @@ class MDListTag(MDRegexTag):
         
 
 
-class MDBlockquotes(MDRegexTag):
-    pass
+class MDQuoteTag(MDRegexTag):
+    def __init__(self):
+        super(MDQuoteTag, self).__init__("quote tag", r"^> ?(.*)")
 
 
 #Need to happen before <p>
@@ -401,12 +395,6 @@ class MDInlineCode(MDTag):
         return self._numberOfConsecutiveCharacters(origStr,"`",startingIndex)
 
 
-            
-
-
-
-
-
 #Parser
 class MDParser:
     def __init__():
@@ -431,161 +419,3 @@ class MDBlockParser:
 
 class MDInlineParser:
     pass
-# Test:
-# h = MDHeaderTag();
-# print h.action("######hahsdf###")
-
-# b = MDBoldTag();
-# print b.action("sDAFSd")
-
-c = MDCodeTag();
-# test_code = """
-#      This is in code
-
-# ###sdfasf###########################
-#     dsfsdaf
-   
-#     safaf
-#       dsfs
-# sdfsdfa
-#     sdfaf
-#    sdsdf
-#     dsfasdf
-#     sdfad
-#     sfdaf
-    
-# """
-# return_str = ""
-# control = None
-# for line in test_code.splitlines(True):
-#   print("%%%%%%" + line)
-#   if (control == None):
-#       print("Control none")
-#       new_str,match,capture = c.action(line)
-#       print("$$Add str:" + new_str)
-#       return_str += new_str
-#       if match:
-#           print("match!Should not do others")
-#       else:
-#           print("Not match!Should do others")
-#       if capture:
-#           control = c
-#           print("capture!")
-#       else:
-#           control = None
-#           print("not capture!")
-#   else:
-#       print("Control is c")
-#       new_str,match,capture = c.action(line)
-#       return_str += new_str
-#       print("$$Add str:" + new_str)
-#       if match:
-#           print("continue match!Should not do others")
-#       else:
-#           print("Not match!Out")
-#       if capture:
-#           control = c
-#           print("continue capture!")
-#       else:
-#           control = None
-#           print("not capture!Out")
-# if control:
-#   new_str,match,capture = c.action("   dsf\n")
-#   return_str += new_str
-# print(return_str)
-
-# inline = MDInlineCode()
-# inline.action("""
-# ``ds`af``
-# ``saf``
-# `
-# `sdf ` `
-# `` ` ``
-# ``` `` ```
-# `d```
-# ```sdaf`` sdasdf``` sdfa`saf``sdvsvzx`` `` sdaf`1sv`svzxc``
-#     """)
-
-
-# inline = MDInlineCode()
-# inline.action("""`d```
-# ``""")
-
-listTag = MDListTag()
-
-test_code = """
-1. sddfavxczvz
-2. safdsa
- 3. sdfafsadf
- 4. fsddfasdf
- sdfsdafas
-     sdfsadfsaf
- - sdfasf
-        - sdfafdas
-    - fdsadfa
-- sdfadfa
-         - fsdafdfsaf
-    - fdsafafas
-fsdfadfafadf
-3. sdfsafdaf
-        5. dsafasdfa
-    4. ssdfdafaf
-3. sdfasdfaf
-             8. sfdasdfda
-    3. sdasdfasf
-
-- sdafdasfd
- - fsdafaf
-     - dasfasf
-
-fsdfsaf
-
-- sdfasdfa
- - dsafa
- 1. sdafdsaf
-         - fdsafa
- - dsafaf
-- sdfadf
-- fsdfaf
-
-sdfsafda
-"""
-return_str = ""
-control = None
-for line in test_code.splitlines(True):
-  print("%%%%%%" + line.strip('\n'))
-  if (control == None):
-      print("Control none")
-      new_str,match,capture = listTag.action(line)
-      print("$$Add str:" + new_str)
-      return_str += new_str
-      if match:
-          print("match!Should not do others")
-      else:
-          print("Not match!Should do others")
-      if capture:
-          control = listTag
-          print("capture!")
-      else:
-          control = None
-          print("not capture!")
-  else:
-      print("Control is c")
-      new_str,match,capture = listTag.action(line)
-      return_str += new_str
-      print("$$Add str:" + new_str)
-      if match:
-          print("continue match!Should not do others")
-      else:
-          print("Not match!Out")
-      if capture:
-          control = listTag
-          print("continue capture!")
-      else:
-          control = None
-          print("not capture!Out")
-if control:
-  new_str,match,capture = listTag.action("   \nsdadfasfd\n")
-  return_str += new_str
-#print(return_str)
-print(listTag.returnStr)
---
+++ test.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+from markdown2html import *
+
+
+# Test:
+# h = MDHeaderTag();
+# print h.action("######hahsdf###")
+
+# b = MDBoldTag();
+# print b.action("sDAFSd")
+
+c = MDCodeTag();
+# test_code = """
+#      This is in code
+
+# ###sdfasf###########################
+#     dsfsdaf
+	 
+#     safaf
+#       dsfs
+# sdfsdfa
+#     sdfaf
+#    sdsdf
+#     dsfasdf
+#     sdfad
+#     sfdaf
+		
+# """
+# return_str = ""
+# control = None
+# for line in test_code.splitlines(True):
+#   print("%%%%%%" + line)
+#   if (control == None):
+#       print("Control none")
+#       new_str,match,capture = c.action(line)
+#       print("$$Add str:" + new_str)
+#       return_str += new_str
+#       if match:
+#           print("match!Should not do others")
+#       else:
+#           print("Not match!Should do others")
+#       if capture:
+#           control = c
+#           print("capture!")
+#       else:
+#           control = None
+#           print("not capture!")
+#   else:
+#       print("Control is c")
+#       new_str,match,capture = c.action(line)
+#       return_str += new_str
+#       print("$$Add str:" + new_str)
+#       if match:
+#           print("continue match!Should not do others")
+#       else:
+#           print("Not match!Out")
+#       if capture:
+#           control = c
+#           print("continue capture!")
+#       else:
+#           control = None
+#           print("not capture!Out")
+# if control:
+#   new_str,match,capture = c.action("   dsf\n")
+#   return_str += new_str
+# print(return_str)
+
+# inline = MDInlineCode()
+# inline.action("""
+# ``ds`af``
+# ``saf``
+# `
+# `sdf ` `
+# `` ` ``
+# ``` `` ```
+# `d```
+# ```sdaf`` sdasdf``` sdfa`saf``sdvsvzx`` `` sdaf`1sv`svzxc``
+#     """)
+
+
+# inline = MDInlineCode()
+# inline.action("""`d```
+# ``""")
+
+listTag = MDListTag()
+
+test_code = """
+1. sddfavxczvz
+2. safdsa
+ 3. sdfafsadf
+ 4. fsddfasdf
+ sdfsdafas
+		 sdfsadfsaf
+ - sdfasf
+				- sdfafdas
+		- fdsadfa
+- sdfadfa
+				 - fsdafdfsaf
+		- fdsafafas
+fsdfadfafadf
+3. sdfsafdaf
+				5. dsafasdfa
+		4. ssdfdafaf
+3. sdfasdfaf
+						 8. sfdasdfda
+		3. sdasdfasf
+
+- sdafdasfd
+ - fsdafaf
+		 - dasfasf
+
+fsdfsaf
+
+- sdfasdfa
+ - dsafa
+ 1. sdafdsaf
+				 - fdsafa
+ - dsafaf
+- sdfadf
+- fsdfaf
+
+sdfsafda
+"""
+return_str = ""
+control = None
+for line in test_code.splitlines(True):
+	print("%%%%%%" + line.strip('\n'))
+	if (control == None):
+			print("Control none")
+			new_str,match,capture = listTag.action(line)
+			print("$$Add str:" + new_str)
+			return_str += new_str
+			if match:
+					print("match!Should not do others")
+			else:
+					print("Not match!Should do others")
+			if capture:
+					control = listTag
+					print("capture!")
+			else:
+					control = None
+					print("not capture!")
+	else:
+			print("Control is c")
+			new_str,match,capture = listTag.action(line)
+			return_str += new_str
+			print("$$Add str:" + new_str)
+			if match:
+					print("continue match!Should not do others")
+			else:
+					print("Not match!Out")
+			if capture:
+					control = listTag
+					print("continue capture!")
+			else:
+					control = None
+					print("not capture!Out")
+if control:
+	new_str,match,capture = listTag.action("   \nsdadfasfd\n")
+	return_str += new_str
+print(return_str)
+#print(listTag.returnStr)
